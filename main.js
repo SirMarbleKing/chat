f3826311ec59a24dc06ed1f1033a2a083693ff6c
SirMarbleKing <98177301+SirMarbleKing@users.noreply.github.com>
2022-01-21 14:16:27 -0600

    express

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("PROCFILE LOADED MAIN.JS");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (request, response) => {
+    response.status(200).send("fuck you");
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
